73668e048a589347488f4488af330edea121ad84
htetws <liu.zify5521@gmail.com>
2024-04-18 17:49:14 +0630

    3: depoly

--- lib/mail.ts
+++ lib/mail.ts
@@ -2,6 +2,8 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+const domain = process.env.NEXT_PUBLIC_APP_URL;
+
 export const sendTwoFactorTokenByEmail = async (
   token: string,
   email: string
@@ -15,7 +17,7 @@ export const sendTwoFactorTokenByEmail = async (
 };
 
 export const sendVerificationEmail = async (token: string, email: string) => {
-  const confirmLink = `https://next-auth-v5-beta-rh76aqe13-htetws-projects.vercel.app/auth/new-verification?token=${token}`;
+  const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
     from: "onboarding@resend.dev",
@@ -26,7 +28,7 @@ export const sendVerificationEmail = async (token: string, email: string) => {
 };
 
 export const sendPasswordResetEmail = async (token: string, email: string) => {
-  const resetLink = `https://next-auth-v5-beta-rh76aqe13-htetws-projects.vercel.app/auth/new-password?token=${token}`;
+  const resetLink = `${domain}/auth/new-password?token=${token}`;
 
   await resend.emails.send({
     from: "onboarding@resend.dev",
